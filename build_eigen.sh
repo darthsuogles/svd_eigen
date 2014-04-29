8507c3214e7e963c5b8b2c9330f98fedb37cb135
Philip Yang <phi@cs.umd.edu>
2014-04-28 22:48:56 -0400

    further modification

--- build_eigen.sh
+++ build_eigen.sh
@@ -3,6 +3,15 @@
 #ver=3.1.4
 ver=3.2.1
 
+script_name=$0
+
+function quit_with()
+{
+    echo "Error: $script_name"    
+    printf ">> $[@]\n"
+    exit
+}
+
 mkdir eigen; cd eigen
 
 if [ ! -d $ver ]; then
@@ -11,9 +20,15 @@ if [ ! -d $ver ]; then
     if [ ! -f $tarball ]; then
 	wget http://bitbucket.org/eigen/eigen/get/$tarball
     fi
+    echo "Uncompressing ... might take a while ... please be patient"
     #fname=`tar tjf $tarball | sed -e 's@/.*@@' | uniq`
     fname=`tar -jxvf $tarball | sed -e 's@/.*@@' | uniq`
-    mv $fname $ver
+    if [ $fname != $ver ]; then 
+	mv $fname $ver
+    fi
+
+    if [ ! -d $ver ]; then quit_with "failed to uncompress $tarball"; fi
+    rm $tarball
 fi
 
 if [ ! -z "$flag_build_eigen" ]; then
